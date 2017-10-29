f8e46ba44f0cd18021e87ec02c38b597c9fb96f6
Bjorn Wickstrom <bjorn@billo.systems>
2017-10-28 23:51:56 -0500

    Initial files.

---
+++ README.md
@@ -0,0 +1,13 @@
+# clj-xbee
+
+*Clojure Wrapper for Digi's Official XBee Java Library*
+
+## Usage
+
+TBD
+
+## License
+
+Copyright © 2017, Billo Systems, Ltd. Co.
+
+Distributed under the Apache License Version 2.0.
---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject clj-xbee "0.1.0-SNAPSHOT"
+  :description "Clojure Wrapper for Digi's Official XBee Java Library"
+  :url "https://github.com/billosys/clj-xbee"
+  :license {
+    :name "Apache License Version 2.0"
+    :url "https://www.apache.org/licenses/LICENSE-2.0"}
+  :dependencies [
+    [com.digi.xbee/xbee-java-library "1.2.1"]
+    [org.clojure/clojure "1.8.0"]])
