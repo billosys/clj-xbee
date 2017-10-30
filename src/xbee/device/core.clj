25b6ce243ec68a758865d144070740d43ebcde4e
Bjorn Wickstrom <bjorn@billo.systems>
2017-10-30 00:30:22 -0500

    Added initial device support.

---
+++ src/xbee/device/core.clj
@@ -0,0 +1,1 @@
+(ns xbee.device.core)
---
+++ src/xbee/device/xbee.clj
@@ -0,0 +1,26 @@
+(ns xbee.device.xbee
+  (:require [xbee.util :as util])
+  (:import (com.digi.xbee.api XBeeDevice)))
+
+(defprotocol XBeeDeviceAPI
+  (close [this])
+  (open [this])
+  (send-broadcast-data [this data]))
+
+(def behaviour
+  {:close #(.close %)
+   :open #(.open %)
+   :send-broadcast-data (fn [this data]
+                         (.sendBroadcastData
+                          this (util/str->bytes data)))})
+
+(extend XBeeDevice
+        XBeeDeviceAPI
+        behaviour)
+
+(defn create-device
+  [port baud-rate]
+  ;; The following line is a work-around for a bug in rxtx, taken from
+  ;; here: https://bugs.launchpad.net/ubuntu/+source/rxtx/+bug/367833
+  (System/setProperty "gnu.io.rxtx.SerialPorts" port)
+  (new XBeeDevice port (util/str->int baud-rate)))
